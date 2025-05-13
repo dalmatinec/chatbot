e09499f0ba84a4c2e8a8aef803edcc0125f47f07
dalmatinec <temirbekov1602@gmail.com>
2025-05-13 19:52:24 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,4 @@
-
+# config.py
+BOT_TOKEN = "7457617673:AAHDfaEBqFgIXdj4sc26Czdr1F_TzPaXZKw"
+DATABASE_URL = "postgresql://neondb_owner:npg_FmX4Syk5MBYe@ep-rapid-snowflake-ab1m2jq6-pooler.eu-west-2.aws.neon.tech/neondb?sslmode=require"
+OWNER_ID = 7745389150
